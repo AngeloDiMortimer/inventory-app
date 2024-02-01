936b7e67668dd6970de7d49b60b573fa71868c1a
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2024-02-01 19:02:53 -0400

    routes and controllers set up

---
+++ routes/item.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router = express.Router();
+const itemController = require('../controllers/itemController');
+
+router.get('/all', itemController.get_item_all);
+
+router.get('/create', itemController.get_item_create);
+
+router.post('/create', itemController.post_item_create);
+
+
+router.get('/:id', itemController.get_item);
+
+router.get('/:id/edit', itemController.get_item_edit);
+
+router.post('/:id/edit', itemController.post_item_edit);
+
+router.get('/:id/delete', itemController.get_item_delete);
+
+router.post('/:id/delete', itemController.post_item_delete);
+
+module.exports = router;
