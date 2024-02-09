90401e0fc17e9ace686e946ac097e23377dec194
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2024-02-09 17:49:49 -0400

    created categories view

---
+++ controllers/categoryController.js
@@ -0,0 +1,27 @@
+const asyncHandler = require('express-async-handler');
+const Category = require('../models/category');
+const Items = require('../models/item');
+const { body, validationResult } = require('express-validator');
+
+const categoryValidation = () => {
+    body('name', 'Name must be between 1 to 20 characters')
+        .trim()
+        .isLength({ min: 1, max: 20})
+        .escape(),
+        body('description', 'Description must be between 1 to 20 characters')
+            .trim()
+            .isLength({ min: 1, max: 20})
+};
+
+module.exports.category = asyncHandler(async (req, res, next) => {
+    res.redirect(req.baseUrl + '/all');
+})
+
+module.exports.get_category_all = asyncHandler(async (req, res, next) => {
+    const categories = await Category.find({}).exec();
+
+    res.render('category/all', {
+        title: 'All Categories',
+        categories
+    })
+});
